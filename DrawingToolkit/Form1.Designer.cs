f1da0c82a243c0e2ab08d8a63b7abb6a9676c797
Renaldi Wahyudiono <32060371+renaldiwahyudiono22@users.noreply.github.com>
2018-10-17 10:35:58 +0700

    Drawing Toolkit

---
+++ DrawingToolkit/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace DrawingToolkit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.circleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rectangleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.circleToolStripMenuItem,
+            this.lineToolStripMenuItem,
+            this.rectangleToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // circleToolStripMenuItem
+            // 
+            this.circleToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control;
+            this.circleToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("circleToolStripMenuItem.Image")));
+            this.circleToolStripMenuItem.Name = "circleToolStripMenuItem";
+            this.circleToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.circleToolStripMenuItem.Text = "Circle";
+            this.circleToolStripMenuItem.Click += new System.EventHandler(this.Circle_Click);
+            // 
+            // lineToolStripMenuItem
+            // 
+            this.lineToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("lineToolStripMenuItem.Image")));
+            this.lineToolStripMenuItem.Name = "lineToolStripMenuItem";
+            this.lineToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.lineToolStripMenuItem.Text = "Line";
+            this.lineToolStripMenuItem.Click += new System.EventHandler(this.Line_Click);
+            // 
+            // rectangleToolStripMenuItem
+            // 
+            this.rectangleToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("rectangleToolStripMenuItem.Image")));
+            this.rectangleToolStripMenuItem.Name = "rectangleToolStripMenuItem";
+            this.rectangleToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.rectangleToolStripMenuItem.Text = "Rectangle";
+            this.rectangleToolStripMenuItem.Click += new System.EventHandler(this.Rectang_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(901, 487);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "DrawingToolkit";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem circleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rectangleToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ DrawingToolkit/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawingToolkit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            objGraphic = panel1.CreateGraphics();
+        }
+
+        // Global Variable
+        int tebal = 4, initialX, initialY;
+        Pen p;
+        //Color wrn, wrn1;
+        Color wrn = Color.Black; 
+        Color wrn1 = Color.Black;
+        private Graphics objGraphic;
+        private bool shouldPaint = false, warna = false;
+        Boolean line, rectang, circle;
+        double px, py, vector, angle;
+        // Circle
+        int cirW, cirL;
+
+        // Line
+        private Point preCoor, newCoor;
+
+        // Rectangle
+        int width, height;
+
+        // Reset Button
+        void reset()
+        {
+            line = false;
+            rectang = false;
+            circle = false;
+        }
+
+        void colour()
+        {
+            warna = true;
+            p = new Pen(wrn);
+            tebal = 5;
+        }
+
+        // Formula
+        void rumusline()
+        {
+            px = newCoor.X; py = newCoor.Y;
+            vector = Math.Sqrt((Math.Pow(px, 2)) + (Math.Pow(py, 2)));
+            angle = Math.Atan(py / px) * 180 / Math.PI;
+            //display();
+        }
+
+        void rumusrectang()
+        {
+            px = width; py = height;
+            vector = px * py;
+            if (rectang == true)
+            { angle = 0; }
+            //display();
+        }
+
+        void rumuscircle()
+        {
+            px = cirW; py = cirL;
+            vector = Math.PI * 0.5 * (cirW + cirL);
+            if (circle == true)
+            { angle = 360; }
+            //display();
+        }
+
+        // Mouse Event
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && warna == true)
+            {
+                if (line == true)
+                {
+                    shouldPaint = true;
+                    preCoor = e.Location; newCoor = preCoor;
+                    panel1.Invalidate();
+                }
+
+                else if (rectang == true)
+                {
+                    shouldPaint = true;
+                    initialX = e.X; initialY = e.Y;
+                    panel1.Invalidate();
+                }
+
+                else if (circle == true)
+                {
+                    shouldPaint = true;
+                    initialX = e.X; initialY = e.Y;
+                    panel1.Invalidate();
+                }
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (shouldPaint == true && warna == true)
+            {
+                if (line == true)
+                {
+                    ControlPaint.DrawReversibleLine(panel1.PointToScreen(preCoor), panel1.PointToScreen(newCoor), wrn1);
+                    newCoor = new Point(e.X, e.Y);
+                    ControlPaint.DrawReversibleLine(panel1.PointToScreen(preCoor), panel1.PointToScreen(newCoor), wrn1);
+                }
+
+                else if (rectang == true)
+                {
+                    this.Refresh();
+                    p.Width = tebal;
+                    width = e.X - initialX;
+                    height = e.Y - initialY;
+                    Rectangle rect = new Rectangle(Math.Min(e.X, initialX),
+                        Math.Min(e.Y, initialY),
+                        Math.Abs(e.X - initialX),
+                        Math.Abs(e.Y - initialY));
+                    objGraphic.DrawRectangle(p, rect);
+                }
+
+                else if (circle == true)
+                {
+                    this.Refresh();
+                    p.Width = tebal;
+                    cirW = Math.Abs(e.X - initialX);
+                    cirL = Math.Abs(e.Y - initialY);
+                    Rectangle rec = new Rectangle(Math.Min(e.X, initialX),
+                        Math.Min(e.Y, initialY),
+                        Math.Abs(e.X - initialX),
+                        Math.Abs(e.Y - initialY));
+                    objGraphic.DrawEllipse(p, rec);
+                }
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (shouldPaint == true && warna == true)
+            {
+                if (line == true)
+                {
+                    p.Width = tebal;
+                    ControlPaint.DrawReversibleLine(panel1.PointToScreen(preCoor), panel1.PointToScreen(newCoor), wrn1);
+                    objGraphic.DrawLine(p, preCoor, newCoor);
+                    rumusline(); shouldPaint = false;
+                }
+
+                else if (rectang == true)
+                { rumusrectang(); shouldPaint = false; }
+
+                else if (circle == true)
+                { rumuscircle(); shouldPaint = false; }
+            }
+        }
+
+        private void resetBackColour()
+        {
+            lineToolStripMenuItem.BackColor = Color.White;
+            circleToolStripMenuItem.BackColor = Color.White;
+            rectangleToolStripMenuItem.BackColor = Color.White;
+        }
+
+        // Menu Shape
+        private void Line_Click(object sender, EventArgs e)
+        {
+            reset();
+            line = true;
+            colour();
+            resetBackColour();
+            lineToolStripMenuItem.BackColor = Color.Aqua;
+        }
+
+        private void Rectang_Click(object sender, EventArgs e)
+        {
+            reset();
+            rectang = true;
+            colour();
+            resetBackColour();
+            rectangleToolStripMenuItem.BackColor = Color.Aqua;
+        }
+
+        private void Circle_Click(object sender, EventArgs e)
+        {
+            reset();
+            circle = true;
+            colour();
+            resetBackColour();
+            circleToolStripMenuItem.BackColor = Color.Aqua;
+        }
+    }
+}
